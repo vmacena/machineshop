8329ad26b250a8eac3e005e3852634297f824794
Vinicius Macena <macena.v@aluno.ifsp.edu.br>
2024-04-02 14:07:40 -0300

    refactor: Added "Transactional" and "NotNull" annotations

--- src/main/java/br/edu/ifsp/pw3/machineshop/entity/Mecanico.java
+++ src/main/java/br/edu/ifsp/pw3/machineshop/entity/Mecanico.java
@@ -9,11 +9,11 @@ import lombok.Data;
 @Embeddable
 @Data
 public class Mecanico {
-
+    @NotNull
     @Column(name = "MECANICO_NOME")
     private String nome;
 
-
+    @NotNull
     @Column(name = "MECANICO_ANOS_DE_EXPERIENCIA")
     private int anosDeExperiencia;
 }
