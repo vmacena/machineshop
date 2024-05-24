b21438d9bb86d8419732850c0863a533b5971d96
vmacena <macena.v@aluno.ifsp.edu.br>
2024-05-24 19:31:18 -0300

    refactor: impl 'ConsertoServiceImpl', refactor controller and service

--- src/main/java/br/edu/ifsp/pw3/machineshop/controller/ConsertoController.java
+++ src/main/java/br/edu/ifsp/pw3/machineshop/controller/ConsertoController.java
@@ -1,17 +1,11 @@
 package br.edu.ifsp.pw3.machineshop.controller;
 
-import br.edu.ifsp.pw3.machineshop.dto.ConsertoDTO;
 import br.edu.ifsp.pw3.machineshop.dto.DadosAtualizacaoDTO;
-import br.edu.ifsp.pw3.machineshop.dto.DadosSimplesDTO;
 import br.edu.ifsp.pw3.machineshop.entity.Conserto;
-import br.edu.ifsp.pw3.machineshop.entity.Mecanico;
-import br.edu.ifsp.pw3.machineshop.entity.Veiculo;
-import br.edu.ifsp.pw3.machineshop.exception.MecanicoResponsavelNullException;
-import br.edu.ifsp.pw3.machineshop.exception.ValidationException;
 import br.edu.ifsp.pw3.machineshop.service.ConsertoService;
 import jakarta.transaction.Transactional;
-import jakarta.validation.ConstraintViolationException;
 import jakarta.validation.Valid;
+import jakarta.validation.ValidationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -22,81 +16,75 @@ import org.springframework.validation.FieldError;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Optional;
+import java.util.List;
 
 @RestController
 @Validated
 public class ConsertoController {
+    private final ConsertoService service;
 
-   private final ConsertoService service;
-
-   public ConsertoController(ConsertoService service) {
-       this.service = service;
-   }
-
-    @PostMapping("/novo-conserto")
-    public ResponseEntity<Conserto> novoConserto(@RequestBody @Valid ConsertoDTO novoConserto) {
-        Mecanico mecanico = new Mecanico(novoConserto.mecanicoResponsavel().nome(), novoConserto.mecanicoResponsavel().anosDeExperiencia());
-        Veiculo veiculo = new Veiculo(novoConserto.veiculo().marca(), novoConserto.veiculo().modelo(), novoConserto.veiculo().ano(), novoConserto.veiculo().cor());
-        Conserto conserto = new Conserto(novoConserto.dataDeEntrada(), novoConserto.dataDeSaida(), mecanico, veiculo);
-        Conserto savedConserto = service.save(conserto);
-        return new ResponseEntity<>(savedConserto, HttpStatus.CREATED);
+    public ConsertoController(ConsertoService service) {
+        this.service = service;
     }
 
-    @GetMapping("/listar-todos")
-    public ResponseEntity<Page<Conserto>> getAllConsertos(Pageable paginacao) {
-        Page<Conserto> consertos = service.findAll(paginacao);
-        return new ResponseEntity<>(consertos, HttpStatus.OK);
+    @PostMapping("/conserto")
+    @Transactional
+    public ResponseEntity<Void> saveConserto(@RequestBody @Valid Conserto conserto, BindingResult result) {
+        errorHandling(result, result.getFieldError());
+        service.save(conserto);
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
-    @GetMapping("/dados-simples")
-    public ResponseEntity<Page<DadosSimplesDTO>> algunsDados(Pageable paginacao){
-        Page<DadosSimplesDTO> dados = service.findAllActive(paginacao).map(DadosSimplesDTO::new);
-        return new ResponseEntity<>(dados, HttpStatus.OK);
+    @GetMapping("/consertos")
+    public List<Conserto> getAllConsertos() {
+        return service.findAll();
+    }
+
+    @GetMapping("/consertos-ativos")
+    public Page<Conserto> getAllActiveConsertos(Pageable paginacao) {
+        return service.findAllActive(paginacao);
+    }
+
+    @GetMapping("/consertos-paginacao")
+    public Page<Conserto> getAllConsertosWithPagination(Pageable paginacao) {
+        return service.findAll(paginacao);
     }
 
     @GetMapping("/conserto/{id}")
     public ResponseEntity<Conserto> getConsertoById(@PathVariable Long id){
-        Optional<Conserto> consertoOptional = service.getById(id);
-        return consertoOptional.map(conserto -> new ResponseEntity<>(conserto, HttpStatus.OK))
-                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
-    }
-
-    @PutMapping("/atualizar-parcial")
-    public ResponseEntity<Conserto> updatePartial(@RequestBody @Valid DadosAtualizacaoDTO dados){
-        Optional<Conserto> consertoOptional = service.getById(dados.id());
-        if(consertoOptional.isPresent()) {
-            service.updatePartial(dados);
-            Conserto updatedConserto = service.getById(dados.id()).get();
-            return new ResponseEntity<>(updatedConserto, HttpStatus.OK);
-        } else {
+        Conserto conserto = service.getById(id);
+        if (conserto == null) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
+        return new ResponseEntity<>(conserto, HttpStatus.OK);
     }
 
-    @PatchMapping("/conserto/{id}")
-    public ResponseEntity<Conserto> updateConserto(@PathVariable Long id, @RequestBody @Valid ConsertoDTO updatedConserto) {
-        Optional<Conserto> consertoOptional = service.getById(id);
-        if(consertoOptional.isPresent()) {
-            Conserto conserto = consertoOptional.get();
-            updatedConserto.updateEntity(conserto);
-            service.save(conserto);
-            return new ResponseEntity<>(conserto, HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
+    @PutMapping("/conserto")
+    @Transactional
+    public ResponseEntity<Void> updatePartial(@RequestBody @Valid DadosAtualizacaoDTO dados, BindingResult result){
+        errorHandling(result, result.getFieldError());
+        service.updatePartial(dados);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @DeleteMapping("/conserto/{id}")
+    @Transactional
     public ResponseEntity<Void> deleteConserto(@PathVariable Long id){
+        service.delete(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PatchMapping("/conserto/desativar/{id}")
+    @Transactional
+    public ResponseEntity<Void> deactivateConserto(@PathVariable Long id){
         service.desativarConserto(id);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
-    @ExceptionHandler(ConstraintViolationException.class)
-    public ResponseEntity<Object> handleConstraintViolationException(ConstraintViolationException ex) {
-        String field = ex.getConstraintViolations().iterator().next().getPropertyPath().toString();
-        String message = ex.getConstraintViolations().iterator().next().getMessage();
-        return new ResponseEntity<>("Erro de validação no campo '" + field + "': " + message, HttpStatus.BAD_REQUEST);
+    private static void errorHandling(BindingResult result, FieldError result1) {
+        if (result.hasErrors()) {
+            String errorMessage = result1.getDefaultMessage();
+            throw new ValidationException(errorMessage);
+        }
     }
 }
