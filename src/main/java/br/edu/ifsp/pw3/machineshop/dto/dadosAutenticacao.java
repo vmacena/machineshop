df7b813d7720dbf46127be5ce54a1a8a46b0caa9
vmacena <macena.v@aluno.ifsp.edu.br>
2024-06-02 21:21:45 -0300

    Revert "chore: implement spring security"
    chore: implement spring security

--- src/main/java/br/edu/ifsp/pw3/machineshop/dto/dadosAutenticacao.java
+++
@@ -1,9 +0,0 @@
-package br.edu.ifsp.pw3.machineshop.dto;
-
-import jakarta.validation.constraints.NotBlank;
-public record dadosAutenticacao(
-        @NotBlank
-        String login,
-        @NotBlank
-        String senha) {
-}
--- src/main/java/br/edu/ifsp/pw3/machineshop/util/security/DadosTokenJWT.java
+++
@@ -1,5 +0,0 @@
-package br.edu.ifsp.pw3.machineshop.util.security;
-
-public record DadosTokenJWT(String token) {
-
-}
